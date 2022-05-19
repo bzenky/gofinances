4cbd4bc08e52aeaa8a63ef53839fba4bbe127968
Zenky <bzenky@gmail.com>
2022-05-18 21:40:22 -0300

    static transactions cards

---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native'
+import { Feather } from '@expo/vector-icons'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+interface TransactionProps {
+    type: 'positive' | 'negative'
+}
+
+export const Container = styled.View`
+    padding: 17px 24px;
+    margin-bottom: 16px;
+    background-color: ${({ theme }) => theme.colors.shape};
+    border-radius: 5px;
+`
+
+export const Title = styled.Text`
+    font-size: ${RFValue(14)}px;
+    font-family: ${({ theme }) => theme.fonts.regular}
+`
+
+export const Amount = styled.Text<TransactionProps>`
+    margin-top: 2px;
+    font-size: ${RFValue(20)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    color: ${({ theme, type }) =>
+        type === 'positive' ? theme.colors.success : theme.colors.attention};
+`
+
+export const Footer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 19px;
+`
+
+export const Category = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    color: ${({ theme }) => theme.colors.text};
+`
+
+export const CategoryName = styled.Text`
+    margin-left: 17px;
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.colors.text};
+`
+
+export const Date = styled.Text`
+    font-size: ${RFValue(14)}px;
+    color: ${({ theme }) => theme.colors.text};
+`
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import {
     Container,
@@ -11,10 +12,53 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+    id: string
+}
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: 'Desenvolvimento de site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: '13/04/2022'
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: 'Hamburgueria Pizzy',
+            amount: 'R$ 59,00',
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: '10/04/2022'
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: 'Aluguel do apartamento',
+            amount: 'R$ 1.200,00',
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: '10/04/2022'
+        }
+    ]
+
     return (
         <Container>
             <Header>
@@ -53,6 +97,16 @@ export function Dashboard() {
                     lastTransaction='01 à 16 de abril'
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     );
 }
